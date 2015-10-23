58d9b1adb887ad9bebbebba8fa6208fdeaa67101
Kamil Kiełbasa <kamil.m.kielbasa@gmail.com>
2015-10-23 21:36:11 +0200

    try to migrate from react-hot-loader to react-transform (not-completed)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 module.exports = {
     context: __dirname + '/app/js',
-    entry: './app.js',
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+        './app.js' // Your appʼs entry point
+    ],
 
     output: {
         filename: 'app.js',
